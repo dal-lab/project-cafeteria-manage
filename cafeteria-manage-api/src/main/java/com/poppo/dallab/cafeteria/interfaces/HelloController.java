95195c43897b4bd0d618b2880c25f3dbe96c73c9
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2019-09-27 22:27:41 +0900

    frontend init

--- cafeteria-manage-api/src/main/java/com/poppo/dallab/cafeteria/interfaces/HelloController.java
+++ cafeteria-manage-api/src/main/java/com/poppo/dallab/cafeteria/interfaces/HelloController.java
@@ -1,15 +1,17 @@
 package com.poppo.dallab.cafeteria.interfaces;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
 
-        return "hello";
+        return "hello cafeteria mgmt";
     }
 
 }
