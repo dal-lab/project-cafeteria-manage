9091766151b3a1d26674c590d6b166f7382f1c79
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2019-10-01 15:44:05 +0900

    MenuPlanControllerTests 수정 - cotroller -> service 구간에서 Menu 도메인 검증 책임 주체 변경(controller -> service)

--- cafeteria-manage-api/src/test/java/com/poppo/dallab/cafeteria/interfaces/MenuPlanControllerTests.java
+++ cafeteria-manage-api/src/test/java/com/poppo/dallab/cafeteria/interfaces/MenuPlanControllerTests.java
@@ -1,10 +1,8 @@
 package com.poppo.dallab.cafeteria.interfaces;
 
 import com.poppo.dallab.cafeteria.applications.MenuPlanService;
-import com.poppo.dallab.cafeteria.domain.Menu;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -12,6 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -44,8 +43,7 @@ public class MenuPlanControllerTests {
                 .andExpect(header().stringValues("Location", "/workDay/2019-09-30/menuPlans"))
                 .andExpect(status().isCreated());
 
-        // TODO: anyList 앞에 꼭 Mockito 붙어야 하는지 확인할 것
-        verify(menuPlanService).addBulkMenu(eq("2019-09-30"), Mockito.<Menu>anyList());
+        verify(menuPlanService).addBulkMenu(eq("2019-09-30"), anyList());
 
     }
 
