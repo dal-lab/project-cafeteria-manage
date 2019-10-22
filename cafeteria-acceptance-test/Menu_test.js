af88e0bd3978fb1b0c83b953b3474348f4807c27
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2019-10-22 12:59:21 +0900

    인수 테스트 수정 - MainPage 인수 테스트 수정 - Menu 관리 페이지 인수 테스트 수정

--- cafeteria-acceptance-test/Menu_test.js
+++ cafeteria-acceptance-test/Menu_test.js
@@ -10,5 +10,5 @@ Scenario('메뉴_입력_페이지_확인', (I) => {
     I.amOnPage('http://localhost:3000/menus');
     I.click({ css: 'button.addMenu' });
     I.fillField('메뉴명', '제육볶음');
-    
+    I.see('제육볶음')
 });
