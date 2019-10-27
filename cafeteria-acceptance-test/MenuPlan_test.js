686a2275f6f32f1309319501bad41bca452598ba
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2019-10-28 08:10:13 +0900

    식단 입력 인수테스트 작성 - 식단 입력 페이지 확인 인수 테스트 작성 - 식단 관리 페이지 입력 후 결과 확인 테스트 작성

---
+++ cafeteria-acceptance-test/MenuPlan_test.js
@@ -0,0 +1,32 @@
+
+Feature('MenuTest');
+
+Scenario('식단관리 관리 페이지 이동 확인', (I) => {
+    I.amOnPage('http://localhost:3333/menuPlans');
+    I.see('식단 관리');
+});
+
+Scenario('식단 입력 테스트(11월)', (I) => {
+    I.amOnPage('http://localhost:3333/menuPlans');
+    I.see('새 식단 추가')
+    I.click('새 식단 추가');
+    I.fillField('month', '10');
+    I.click('식단 추가');
+    I.see('2019-11-01');
+});
+
+// Scenario('메뉴 삭제 테스트(제육볶음)', (I) => {
+//     I.amOnPage('http://localhost:3333/menus');
+//     I.click('제육볶음');
+//     I.click('메뉴 삭제')
+//     I.dontSee('제육볶음')
+// });
+
+// Scenario('메뉴 상세 테스트(제육볶음)', (I) => {
+//     I.amOnPage('http://localhost:3333/menus');
+//     I.click('제육볶음');
+//     I.see('메뉴 상세');
+//     I.fillField('menuName', '오징어볶음');
+//     I.fillField('메뉴 수정');
+//     I.see('오징어볶음')
+// });
