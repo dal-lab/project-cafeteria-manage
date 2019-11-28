3d56d8281f1655f486eba9a92de1c4f67c67915f
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2019-11-29 08:11:45 +0900

    필요없는 코드 제거

--- cafeteria-manage-api/src/main/java/com/poppo/dallab/cafeteria/domain/WorkDayRepository.java
+++ cafeteria-manage-api/src/main/java/com/poppo/dallab/cafeteria/domain/WorkDayRepository.java
@@ -1,6 +1,5 @@
 package com.poppo.dallab.cafeteria.domain;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
@@ -13,7 +12,4 @@ public interface WorkDayRepository extends JpaRepository<WorkDay, Long> {
     Boolean existsByDateBetween(LocalDate startDate, LocalDate endDate);
 
     List<WorkDay> findByDateBetween(LocalDate startDate, LocalDate endDate);
-
-    List<WorkDay> findAllByDateBetweenAndDayNotLikeAndDayNotLike(
-            LocalDate startDate, LocalDate endDate, String saturday, String sunday, Pageable pageable);
 }
--- cafeteria-manage-api/src/test/java/com/poppo/dallab/cafeteria/domain/WorkDayRepositoryTests.java
+++ cafeteria-manage-api/src/test/java/com/poppo/dallab/cafeteria/domain/WorkDayRepositoryTests.java
@@ -4,13 +4,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.util.List;
-import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -36,32 +33,6 @@ public class WorkDayRepositoryTests {
         Boolean result = workDayRepository.existsByDateBetween(startDate, endDate);
 
         assertThat(result).isTrue();
-    }
-
-    @Test
-    public void 저장된_workDay_중_제시된_범위_내에_속하는_결과를_가져오고_주말을_제외했는가() {
-
-        IntStream.rangeClosed(1, 30).forEach(i -> {
-
-            LocalDate date = LocalDate.of(2019, 11, i);
-
-            WorkDay workDay = WorkDay.builder()
-                    .day(date.getDayOfWeek().name())
-                    .date(date)
-                    .build();
-
-            workDayRepository.save(workDay);
-        });
-
-        LocalDate startDate = LocalDate.of(2019, 11, 1);
-        LocalDate endDate = LocalDate.of(2019, 11, 30);
-
-        PageRequest pageable = PageRequest.of(0, 30);
-
-        List<WorkDay> weekendFilteredWorkDay = workDayRepository.findAllByDateBetweenAndDayNotLikeAndDayNotLike(
-                startDate, endDate, "SATURDAY", "SUNDAY", pageable);
-
-        assertThat(weekendFilteredWorkDay).hasSize(21);
     }
 
 }
