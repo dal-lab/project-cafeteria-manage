583b1d01beaa0738ab7ddf808bda32fe1244c420
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-03-07 17:06:02 +0900

    메인페이지 수정
    - 화면에 맞춰 꽉 차도록 사진 배율 수정

--- cafeteria-manage-web/src/components/Home.vue
+++ cafeteria-manage-web/src/components/Home.vue
@@ -4,18 +4,20 @@
             <img :src="'../images/mainPage.jpg'" class="main-page">
         </div>
     </div>
-    
+
 </template>
 
 <script>
 export default {
-    
+
 }
 </script>
 
 <style>
+
 .main-page {
-    width: 100%;
-    height: 50%;
+  position: absolute; top:0; left: 0;
+  width: 100%;
+  height: 100%;
 }
 </style>
